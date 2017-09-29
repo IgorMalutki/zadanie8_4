88faa78b9e5944fbb9230af8c89d3e456f90c20d
IgorMalutki <ireneuszzadlo@gmail.com>
2017-09-29 10:39:42 +0200

    zadanie 8.4

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+
+var name = prompt('Enter your name'); /* deklaracja zmiennej o nazwie "name" ,przypisanie jej wartości którą zwraca funkacja prompt*/
+// alert('Hello, ' + name);
+console.log('wartość zmiennej name to : ' + name); // funkcja pozwalająca wyświetlić aktualną wartość zmiennej "name"
